70495277221153d684790ce9f0abde8f5e9a2042
Honey171 <103601089+Honey171@users.noreply.github.com>
2022-12-04 18:15:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/home"
+        element={<Home />}
+      />
+      <Route
+        path="/product/:id"
+        element={<Product />}
+      />
+      <Route
+        path="/cart"
+        element={<Cart />}
+      />
+      <Route
+        path="/signUp"
+        element={<SignUp />}
+      />
+      <Route
+        path="/signIn"
+        element={<SignIn />}
+      />
+    </Routes>
   );
 }
 
